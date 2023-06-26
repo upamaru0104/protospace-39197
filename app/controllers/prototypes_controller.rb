198cad27f1f0343c23630baea8a069161ce7f048
upamaru0104 <upakame0104@gmail.com>
2023-06-26 14:57:02 +0900

    ログインの有無での表示の切り替え

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,11 @@
 class PrototypesController < ApplicationController
-  def index
-  end
+  # before_action :authenticate_user!, except: [:index, :show]
+	def index
+	end
+
+	private
+
+	def prototype_params
+		params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user&.id)
+	end
 end
