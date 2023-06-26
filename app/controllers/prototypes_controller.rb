51cb0836e21d063f19a3ea68034ed5c7e5e8b840
upamaru0104 <upakame0104@gmail.com>
2023-06-26 15:34:56 +0900

    画像の保存を許可するストロングパラメーター記述

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,6 +6,6 @@ class PrototypesController < ApplicationController
 	private
 
 	def prototype_params
-		params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user&.id)
+		params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
 	end
 end
